02dc9cabf023473d28a098757067a2290ec0343d
Nicholas <nick.orsi1992@gmail.com>
2024-01-03 16:30:07 -0800

    Added GET route items/name to handle retrieving an item of the same passed in name from the db and displaying the item. If name isn't in db then pass on to error handling route.

--- routes/items.js
+++ routes/items.js
@@ -14,6 +14,10 @@ router.get("/", function (req, res, next) {
   return res.json(db);
 });
 
+/**
+ * POST /items: accept JSON body, add item, and return it
+*/
+
 router.post("/", function (req, res, next) {
   if (req.body === undefined) {
     throw new BadRequestError();
@@ -24,5 +28,19 @@ router.post("/", function (req, res, next) {
 
   return res.status(201).json({ added: req.body });
 });
+
+/**
+ * GET /items/:name: return single item
+*/
+
+router.get("/:name", function (req, res, next) {
+  const items = db.items;
+  for(const item of items) {
+    if(item.name === `${req.params.name}`) {
+      return res.json(item);
+    }
+  }
+  return next();
+})
 
 module.exports = router;
